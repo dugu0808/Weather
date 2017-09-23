8657488fd49d1d7a9bdc7b7e777eb735bad65756
dugu0808 <450234523@qq.com>
2017-09-23 20:27:01 +0800

    Toolbar

---
+++ Weather/app/src/main/java/me/keliu/weather/MainActivity.java
@@ -0,0 +1,17 @@
+package me.keliu.weather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by KE on 2017/9/21.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState{
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
