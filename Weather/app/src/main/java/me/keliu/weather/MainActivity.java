e5b8960f77bd9c7dc02ba579b8fdcadc1c3efd48
dugu0808 <450234523@qq.com>
2017-09-25 17:51:53 +0800

    create project

--- Weather/app/src/main/java/me/keliu/weather/MainActivity.java
+++ Weather/app/src/main/java/me/keliu/weather/MainActivity.java
@@ -10,7 +10,7 @@ import android.os.Bundle;
 public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState{
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
     }
