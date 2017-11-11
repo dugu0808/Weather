12a90db6a67b7e8d8ff01fc0a529d9cbbf25d4b2
Liu Ke <450234523@qq.com>
2017-11-11 20:05:54 +0800

    update application

--- Weather/app/src/main/java/me/keliu/weather/SelectCity.java
+++ Weather/app/src/main/java/me/keliu/weather/SelectCity.java
@@ -4,7 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.keliu.app.MyApplication;
+import me.keliu.bean.City;
 
 /**
  * Created by KE on 2017/10/18.
@@ -12,6 +20,10 @@ import android.widget.ImageView;
 
 public  class SelectCity extends Activity implements View.OnClickListener{
     private ImageView mBackBtn;
+    private ListView cityListLv;
+    private List<City>mCityList;
+    private MyApplication mApplication;
+    private ArrayList<String> mArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -21,6 +33,15 @@ public  class SelectCity extends Activity implements View.OnClickListener{
         mBackBtn = (ImageView)findViewById(R.id.title_back);
         mBackBtn.setOnClickListener(this);
 
+        mApplication = (MyApplication)getApplication();
+        mCityList = mApplication.getCityList();
+        mArrayList = new ArrayList<String>();
+        for (int i = 0; i<mCityList.size(); i++) {
+            String cityName = mCityList.get(i).getCity();
+            mArrayList.add(cityName);
+        }
+        cityListLv = (ListView)findViewById(R.id.selectcity_lv);
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(SelectCity.this,android.R.layout.simple_list_item_1,mArrayList);
     }
 
     @Override
